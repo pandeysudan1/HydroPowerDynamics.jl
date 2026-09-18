83a4c4fa49d40ca78b6782c1aeac8b93e3c627c8
pandeysudan1 <pandeysudan1@gmail.com>
2026-09-19 00:55:04 +0200

    test: include reservoir tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 
 @testset "HydroPowerDynamics" begin
     include("test_connectors.jl")
+    include("test_reservoir.jl")
     include("test_utils.jl")
     include("test_francis_step.jl")
     include("test_openhpl_surge_tank.jl")
