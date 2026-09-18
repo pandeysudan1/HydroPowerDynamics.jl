d168c45fbea1de466314bbc79c6418405cab28f2
pandeysudan1 <pandeysudan1@gmail.com>
2026-09-19 01:08:15 +0200

    test: include RigidPipe analytical validation

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using Test
     include("test_connectors.jl")
     include("test_reservoir.jl")
     include("test_trollheim_reservoir_validation.jl")
+    include("test_rigid_pipe.jl")
     include("test_utils.jl")
     include("test_francis_step.jl")
     include("test_openhpl_surge_tank.jl")
