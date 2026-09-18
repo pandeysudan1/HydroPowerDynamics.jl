545bd486c8ff6a5fc8baa135465ee2e43896bb2b
pandeysudan1 <pandeysudan1@gmail.com>
2026-09-19 01:41:13 +0200

    test: include TurbineLookup validation

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ using Test
     include("test_trollheim_reservoir_validation.jl")
     include("test_rigid_pipe.jl")
     include("test_standard_signals.jl")
+    include("test_turbine_lookup.jl")
     include("test_utils.jl")
     include("test_francis_step.jl")
     include("test_openhpl_surge_tank.jl")
