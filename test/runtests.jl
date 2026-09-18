c073568fc64a0f385b0a95c323827939a24aae8f
pandeysudan1 <pandeysudan1@gmail.com>
2026-09-19 01:02:09 +0200

    test: include Trollheim reservoir analytical validation

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 @testset "HydroPowerDynamics" begin
     include("test_connectors.jl")
     include("test_reservoir.jl")
+    include("test_trollheim_reservoir_validation.jl")
     include("test_utils.jl")
     include("test_francis_step.jl")
     include("test_openhpl_surge_tank.jl")
