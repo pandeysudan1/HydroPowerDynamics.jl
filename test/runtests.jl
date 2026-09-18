edad339c7342d4eae8826500fad6684e9fafcd12
pandeysudan1 <pandeysudan1@gmail.com>
2026-09-19 00:51:53 +0200

    test: include physical connector tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@
 using Test
 
 @testset "HydroPowerDynamics" begin
+    include("test_connectors.jl")
     include("test_utils.jl")
     include("test_francis_step.jl")
     include("test_openhpl_surge_tank.jl")
