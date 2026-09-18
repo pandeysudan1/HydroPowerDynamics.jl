086c22cc98cc8513e7c850677404b74d6edf4416
pandeysudan1 <pandeysudan1@gmail.com>
2026-09-19 01:19:37 +0200

    test: include standard signal validation

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,7 @@ using Test
     include("test_reservoir.jl")
     include("test_trollheim_reservoir_validation.jl")
     include("test_rigid_pipe.jl")
+    include("test_standard_signals.jl")
     include("test_utils.jl")
     include("test_francis_step.jl")
     include("test_openhpl_surge_tank.jl")
